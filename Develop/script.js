0cd15116a21797a6031f525e6e0defaf755a70f6
wctestbank <wc.businessmail@gmail.com>
2022-01-16 22:46:26 -0500

    Added simple password generation

--- Develop/script.js
+++ Develop/script.js
@@ -10,41 +10,47 @@ var generateBtn = document.querySelector("#generate");
 
 // Prompts user for criteria and generates password
 function generatePassword() {
-  var passwordPool=[]; // Array of possible characters for password to generate from
-  var password = "TEST";
+  var passwordPool = []; // Array of possible characters for password to generate from
+  var password = "";
 
   //Prompts user for each character type and creates pool of characters to generate from
   var passwordLength = prompt("Enter the password length (8 - 128 Characters):");
-  if (passwordLength > 128 || passwordLength <8 || isNaN(passwordLength)){
+  if (passwordLength > 128 || passwordLength < 8 || isNaN(passwordLength)) {
     return "Not Valid Please Try Again";
   }
 
   var lowerCheck = confirm("Do you want lowercase letters in your password?");
-  if (lowerCheck == true){
+  if (lowerCheck == true) {
     passwordPool = passwordPool.concat(lowercaseArray);
   }
 
   var upperCheck = confirm("Do you want uppercase letters in your password?");
-  if (upperCheck == true){
+  if (upperCheck == true) {
     passwordPool = passwordPool.concat(uppercaseArray);
   }
 
   var numerialCheck = confirm("Do you want numbers in your password?");
-  if (numerialCheck == true){
+  if (numerialCheck == true) {
     passwordPool = passwordPool.concat(numerialsArray);
   }
 
   var specialCheck = confirm("Do you want special characters in your password?");
-  if (specialCheck == true){
+  if (specialCheck == true) {
     passwordPool = passwordPool.concat(specialsArray);
   }
 
-console.log(passwordPool);
+  // Check if at least 1 criteria was added
+  if (passwordPool.length == 0) {
+    return "Please Select at least 1 criteria, Try Again";
+  }
+
+  // Password generation
+  for (var i = 0; i < passwordLength; i++) {
+    password = password + passwordPool[Math.floor((Math.random() * passwordPool.length))];
+  }
 
   return password;
 }
-
-
 
 
 // Write password to the #password input
